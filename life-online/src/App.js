a6c74f4002d816ccb8b7e4090f5cdf869e5133d2
Erick Weil <erick.weil@ifro.edu.br>
2022-11-21 17:55:48 -0300

    arrumado IP para kubernetes

--- life-online/src/App.js
+++ life-online/src/App.js
@@ -7,7 +7,8 @@ import JogoDaVida from './pages/JogoDaVida';
 function App() {
   //const APIURL = "localhost:9090";
   
-  const APIURL = "192.168.1.112:31001";
+  //const APIURL = "192.168.1.112:31001";
+  const APIURL = "172.22.220.35:31001";
   const BASENAME = window.PUBLIC_URL && window.PUBLIC_URL.length > 0 ? window.PUBLIC_URL : "/";
   return (
     <BrowserRouter basename={BASENAME}>
