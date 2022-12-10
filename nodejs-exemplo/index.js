ba312b1e37660424452367d5b77873b2b539cc9b
Erick Weil <erick.weil@ifro.edu.br>
2022-12-09 23:14:59 -0300

    Adicionado pagina terminal

--- nodejs-exemplo/index.js
+++ nodejs-exemplo/index.js
@@ -5,6 +5,9 @@ const app = express();
 const { lookup } = require('dns').promises;
 const { hostname } = require('os');
 
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec)
+
 async function getMyIPAddress(options) {
   return (await lookup(hostname(), options))
     .address;
@@ -34,17 +37,55 @@ app.use(express.static('public'));
 // Gerado uma vez só na inicialização
 const randomNumber = Math.floor(Math.random() * 10000) + 1;
 
-app.get("/", async (req, res) => {
-
-    const info = {
+const getInfo = async (req,res) => {
+    return {
+        numero: randomNumber,
         ip: res.socket.remoteAddress,
         user_agent: req.get('User-Agent'),
         path: req.originalUrl,
         host_ip: await getMyIPAddress()
     };
+}
 
-    res.render("index",{ 
-        numero:randomNumber,
+const execute = async (command) => {
+    try{
+    return await exec(command);
+    }
+    catch(err) {
+        return {stdout:"",stderr:""+err}
+    }
+};
+
+app.get("/terminal", async (req, res) => {
+
+    const info = await getInfo(req,res)
+
+    res.render("terminal",{
+        titulo:"Terminal", 
+        conteudo:"Digite o comando e ele será exibido abaixo:",
+        cmd:"",
+        info:info
+    });
+})
+
+
+app.post("/terminal", async (req, res) => {
+
+    const info = await getInfo(req,res)
+    const output = await execute(req.body.cmd)
+    res.render("terminal",{
+        titulo:"Terminal", 
+        conteudo:"Digite o comando e ele será exibido abaixo:",
+        cmd:(""+output.stdout)+(""+output.stderr),
+        info:info
+    });
+})
+
+app.get("/", async (req, res) => {
+
+    const info = await getInfo(req,res)
+
+    res.render("index",{
         titulo:"Seja bem vindo!!", 
         conteudo:"Obrigado por acessar esta página",
         info:info
